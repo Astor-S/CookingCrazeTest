acfdf7865ca4bf81afc9f9a99eed08b89e142d2b
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-04 19:48:37 +0300

    refactor:  improving code style and code quality
    1) correction of order namespaces
    2) correction formatting  in class
    3) explicitly specifying access modifiers
    4) replacing public fields with private with [SerializeField] attribute and creating properties
    5) add HandleFoodCooking method
    6)  replacing the operator "!" with an explicit instruction
    7) general code readability improvements

--- Assets/Scripts/Kitchen/FoodPlace.cs
+++ Assets/Scripts/Kitchen/FoodPlace.cs
@@ -1,95 +1,94 @@
+using System;
 using UnityEngine;
 
-using System;
+namespace CookingPrototype.Kitchen
+{
+	public class FoodPlace : AbstractFoodPlace
+	{
+		[SerializeField] private bool _сook = false;
+		[SerializeField] private float _cookTime = 0f;
+		[SerializeField] private float _overcookTime = 0f;
 
-namespace CookingPrototype.Kitchen {
-	public class FoodPlace : AbstractFoodPlace {
-		public bool  Cook         = false;
-		public float CookTime     = 0f;
-		public float OvercookTime = 0f;
+		private float _timer = 0f;
 
 		public event Action FoodPlaceUpdated;
 
-		float _timer      = 0f;
-
-		public Food CurFood   { get; private set; } = null;
+		public Food CurrentFood { get; private set; } = null;
 		public bool IsCooking { get; private set; } = false;
 
-		public bool IsFree { get { return CurFood == null; } }
+		public bool IsFree => CurrentFood == null;
 
-		public float TimerNormalized {
-			get {
-				if ( IsFree || !Cook || !IsCooking ) {
+		public float TimerNormalized
+		{
+			get
+			{
+				if (IsFree || _сook == false || IsCooking == false)
 					return 0f;
-				}
-				if ( CurFood.CurStatus == Food.FoodStatus.Raw ) {
-					return _timer / CookTime;
-				}
-				return _timer / OvercookTime;
+				if (CurrentFood.CurrentStatus == Food.FoodStatus.Raw)
+					return _timer / _cookTime;
+
+				return _timer / _overcookTime;
 			}
 		}
 
-		void Update() {
-			if ( IsFree || !Cook || !IsCooking ) {
+		private void Update()
+		{
+			if (IsFree || _сook == false || IsCooking == false)
 				return;
-			}
 
 			_timer += Time.deltaTime;
-			switch ( CurFood.CurStatus ) {
-				case Food.FoodStatus.Raw: {
-					if ( _timer > CookTime ) {
-						CurFood.CookStep();
-						_timer = 0f;
-						if ( OvercookTime <= 0f ) {
-							IsCooking = false;
-						}
-						FoodPlaceUpdated?.Invoke();
-					}
+
+			switch (CurrentFood.CurrentStatus)
+			{
+				case Food.FoodStatus.Raw:
+				case Food.FoodStatus.Cooked:
+					HandleFoodCooking();
 					break;
-				}
-				case Food.FoodStatus.Cooked: {
-					if ( _timer > OvercookTime ) {
-						CurFood.CookStep();
-						_timer = 0f;
-						IsCooking = false;
-						FoodPlaceUpdated?.Invoke();
-					}
-					break;
-				}
 			}
 		}
 
-		public override bool TryPlaceFood(Food food) {
-			if ( !IsFree ) {
+		public override bool TryPlaceFood(Food food)
+		{
+			if (IsFree == false)
 				return false;
-			}
 
-			CurFood = food;
-			if ( Cook && CurFood.CurStatus != (Food.FoodStatus.Overcooked) ) {
+			CurrentFood = food;
+
+			if (_сook && CurrentFood.CurrentStatus != Food.FoodStatus.Overcooked)
 				IsCooking = true;
-			}
 
 			FoodPlaceUpdated?.Invoke();
+
 			return true;
 		}
 
-		public Food ExtractFood() {
-			Food res = CurFood;
-			CurFood = null;
+		public Food ExtractFood()
+		{
+			Food foodToExtract = CurrentFood;
+			CurrentFood = null;
 
 			FoodPlaceUpdated?.Invoke();
-			return res;
+
+			return foodToExtract;
 		}
 
-
-		/// <summary>
-		/// Освобождаем место
-		/// </summary>
-		public override void FreePlace() {
-			CurFood   = null;
-			_timer    = 0f;
+		public override void FreePlace()
+		{
+			CurrentFood = null;
+			_timer = 0f;
 			IsCooking = false;
 			FoodPlaceUpdated?.Invoke();
+		}
+
+		private void HandleFoodCooking()
+		{
+			if ( _timer > (CurrentFood.CurrentStatus == Food.FoodStatus.Raw ? _cookTime : _overcookTime))
+			{
+				CurrentFood.CookStep();
+				_timer = 0f;
+				IsCooking = (_overcookTime > 0f && CurrentFood.CurrentStatus == Food.FoodStatus.Cooked);
+				FoodPlaceUpdated?.Invoke();
+			}
 		}
 	}
 }
