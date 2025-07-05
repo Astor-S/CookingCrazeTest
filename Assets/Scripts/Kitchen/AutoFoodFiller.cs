f4aeaf03b4be6dbdb845aa0ad7ac27212d620474
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-05 22:48:52 +0300

    refactor:  improving code style and code quality
    1) correction of order namespaces
    2) correction formatting  in class
    3) explicitly specifying access modifiers
    4) replacing public fields with private with [SerializeField] attribute
    5) specifying an explicit type instead of "var"

--- Assets/Scripts/Kitchen/AutoFoodFiller.cs
+++ Assets/Scripts/Kitchen/AutoFoodFiller.cs
@@ -1,16 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections.Generic;
+namespace CookingPrototype.Kitchen
+{
+	public sealed class AutoFoodFiller : MonoBehaviour
+	{
+		[SerializeField] private string _foodName;
+		[SerializeField] private List<AbstractFoodPlace> _places = new List<AbstractFoodPlace>();
 
-namespace CookingPrototype.Kitchen {
-	public sealed class AutoFoodFiller : MonoBehaviour {
-		public string                  FoodName = null;
-		public List<AbstractFoodPlace> Places   = new List<AbstractFoodPlace>();
-
-		void Update() {
-			foreach ( var place in Places ) {
-				place.TryPlaceFood(new Food(FoodName));
-			}
+		private void Update()
+		{
+			foreach (AbstractFoodPlace place in _places)
+				place.TryPlaceFood(new Food(_foodName));
 		}
 	}
 }
