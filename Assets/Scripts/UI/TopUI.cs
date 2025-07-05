a97a92a368df69a3920e74020e7553d68aeb1ca9
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-05 14:51:49 +0300

    refactor: improving code style and code quality
    1) correction formatting  in class
    2) replacing public fields with private with [SerializeField] attribute
    3) explicitly specifying access modifiers
    4) specifying an explicit type instead of "var"
    5) general code readability improvements

--- Assets/Scripts/UI/TopUI.cs
+++ Assets/Scripts/UI/TopUI.cs
@@ -1,42 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
-
 using CookingPrototype.Controllers;
-
 using TMPro;
 
-namespace CookingPrototype.UI {
-	public sealed class TopUI : MonoBehaviour {
-		public Image    OrdersBar          = null;
-		public TMP_Text OrdersCountText    = null;
-		public TMP_Text CustomersCountText = null;
+namespace CookingPrototype.UI
+{
+	public sealed class TopUI : MonoBehaviour
+	{
+		[SerializeField] private Image _ordersBar;
+		[SerializeField] private TMP_Text _ordersCountText;
+		[SerializeField] private TMP_Text _customersCountText;
 
-		void Start() {
-			GameplayController .Instance.TotalOrdersServedChanged       += OnOrdersChanged;
+		private void Start()
+		{
+			GameplayController.Instance.TotalOrdersServedChanged += OnOrdersChanged;
 			CustomersController.Instance.TotalCustomersGeneratedChanged += OnCustomersChanged;
 			OnOrdersChanged();
 			OnCustomersChanged();
 		}
 
-		void OnDestroy() {
-			if ( GameplayController.Instance ) {
+		private void OnDestroy()
+		{
+			if (GameplayController.Instance)
 				GameplayController.Instance.TotalOrdersServedChanged -= OnOrdersChanged;
-			}
 
-			if ( CustomersController.Instance ) {
+			if (CustomersController.Instance)
 				CustomersController.Instance.TotalCustomersGeneratedChanged -= OnCustomersChanged;
-			}
 		}
 
-		void OnCustomersChanged() {
-			var cc = CustomersController.Instance;
-			CustomersCountText.text = (cc.CustomersTargetNumber - cc.TotalCustomersGenerated).ToString();
+		private void OnCustomersChanged()
+		{
+			CustomersController customerController = CustomersController.Instance;
+			_customersCountText.text = (customerController.CustomersTargetNumber - customerController.TotalCustomersGenerated).ToString();
 		}
 
-		void OnOrdersChanged() {
-			var gc = GameplayController.Instance;
-			OrdersCountText.text = $"{gc.TotalOrdersServed}/{gc.OrdersTarget}";
-			OrdersBar.fillAmount = (float) gc.TotalOrdersServed / gc.OrdersTarget;
+		private void OnOrdersChanged()
+		{
+			GameplayController gameplayController = GameplayController.Instance;
+			_ordersCountText.text = $"{gameplayController.TotalOrdersServed}/{gameplayController.OrdersTarget}";
+			_ordersBar.fillAmount = (float) gameplayController.TotalOrdersServed / gameplayController.OrdersTarget;
 		}
 	}
 }
