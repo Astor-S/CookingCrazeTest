6ed5d223570bd9510d494cb8004cb522972dd394
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-05 23:25:29 +0300

    refactor: improving code style and code quality
    1) correction of order namespaces
    2) correction formatting  in class
    3) explicitly specifying access modifiers
    4) replacing public fields with private with [SerializeField] attribute
    5) general code readability improvements

--- Assets/Scripts/Kitchen/FoodPresenter.cs
+++ Assets/Scripts/Kitchen/FoodPresenter.cs
@@ -1,77 +1,91 @@
+using System;
 using UnityEngine;
 
-using System;
-
-namespace CookingPrototype.Kitchen {
-	public sealed class FoodPresenter : MonoBehaviour {
+namespace CookingPrototype.Kitchen
+{
+	public sealed class FoodPresenter : MonoBehaviour
+	{
 		[Serializable]
-		public class FoodVisualizersSet {
-			public GameObject     Empty                = null;
-			public FoodVisualizer RawVisualizer        = null;
-			public FoodVisualizer CookedVisualizer     = null;
-			public FoodVisualizer OvercookedVisualizer = null;
+		public class FoodVisualizersSet
+		{
+			[SerializeField] private GameObject _empty;
+			[SerializeField] private FoodVisualizer _rawVisualizer;
+			[SerializeField] private FoodVisualizer _cookedVisualizer;
+			[SerializeField] private FoodVisualizer _overcookedVisualizer;
 
-			public void Hide() {
-				if ( Empty != null ) {
-					Empty.SetActive(false);
-				}
-				RawVisualizer       ?.SetEnabled(false);
-				CookedVisualizer    ?.SetEnabled(false);
-				OvercookedVisualizer?.SetEnabled(false);
+			public void Hide()
+			{
+				if (_empty != null)
+					_empty.SetActive(false);
+
+				_rawVisualizer?.SetEnabled(false);
+				_cookedVisualizer?.SetEnabled(false);
+				_overcookedVisualizer?.SetEnabled(false);
 			}
 
-			public void ShowEmpty() {
+			public void ShowEmpty()
+			{
 				Hide();
-				if ( Empty != null ) {
-					Empty.SetActive(true);
-				}
+
+				if ( _empty != null )
+					_empty.SetActive(true);
 			}
 
-			public void ShowStatus(Food.FoodStatus status) {
+			public void ShowStatus(Food.FoodStatus status)
+			{
 				Hide();
-				switch ( status ) {
-					case Food.FoodStatus.Raw: {
-						RawVisualizer?.SetEnabled(true);
+
+				switch (status)
+				{
+					case Food.FoodStatus.Raw:
+					{
+						_rawVisualizer?.SetEnabled(true);
 						return;
 					}
-					case Food.FoodStatus.Cooked: {
-						CookedVisualizer?.SetEnabled(true);
+					case Food.FoodStatus.Cooked:
+					{
+						_cookedVisualizer?.SetEnabled(true);
 						return;
 					}
-					case Food.FoodStatus.Overcooked: {
-						OvercookedVisualizer?.SetEnabled(true);
+					case Food.FoodStatus.Overcooked:
+					{
+						_overcookedVisualizer?.SetEnabled(true);
 						return;
 					}
 				}
 			}
 		}
 
-		public string             FoodName = string.Empty;
-		public FoodVisualizersSet Set      = null;
-		public FoodPlace          Place    = null;
+		[SerializeField] private string _foodName = string.Empty;
+		[SerializeField] private FoodVisualizersSet _set;
+		[SerializeField] private FoodPlace _place;
 
-		void Start() {
-			Set?.Hide();
-			if ( Place ) {
-				Place.FoodPlaceUpdated += OnFoodPlaceUpdated;
-			}
+		private void Start()
+		{
+			_set?.Hide();
+
+			if (_place)
+				_place.FoodPlaceUpdated += OnFoodPlaceUpdated;
 		}
 
-		void OnDestroy() {
-			if ( Place ) {
-				Place.FoodPlaceUpdated -= OnFoodPlaceUpdated;
-			}
+		private void OnDestroy()
+		{
+			if (_place)
+				_place.FoodPlaceUpdated -= OnFoodPlaceUpdated;
 		}
 
-		void OnFoodPlaceUpdated() {
-			if ( Place.IsFree ) {
-				Set?.ShowEmpty();
-			} else {
-				if ( Place.CurFood.Name == FoodName ) {
-					Set?.ShowStatus(Place.CurFood.CurStatus);
-				} else {
-					Set?.Hide();
-				}
+		private void OnFoodPlaceUpdated()
+		{
+			if (_place.IsFree)
+			{
+				_set?.ShowEmpty();
+			}
+			else
+			{
+				if (_place.CurrentFood.Name == _foodName)
+					_set?.ShowStatus(_place.CurrentFood.CurrentStatus);
+				else
+					_set?.Hide();
 			}
 		}
 	}
