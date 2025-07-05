8441959a4570ec52bbaea2b75e2a267aa7dd470f
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-05 17:12:29 +0300

    refactor: add new logic, improving code style,code quality
    1) add new logic related to StartWindow
    2) change logic related to PauseHandler
    3) correction formatting  in class
    4) replacing public fields with private with [SerializeField] attribute
    5) explicitly specifying access modifiers
    6) specifying an explicit type instead of "var"
    7) replacing the operator "!" with an explicit instruction
    8) general code readability improvements

--- Assets/Scripts/Controllers/GameplayController.cs
+++ Assets/Scripts/Controllers/GameplayController.cs
@@ -1,104 +1,130 @@
 using System;
-
 using UnityEngine;
-
 using CookingPrototype.Kitchen;
 using CookingPrototype.UI;
-
 using JetBrains.Annotations;
 
-namespace CookingPrototype.Controllers {
-	public sealed class GameplayController : MonoBehaviour {
-		public static GameplayController Instance { get; private set; }
+namespace CookingPrototype.Controllers
+{
+	public sealed class GameplayController : MonoBehaviour
+	{
+		[SerializeField] private GameObject _tapBlock;
+		[SerializeField] private WinWindow  _winWindow;
+		[SerializeField] private LoseWindow _loseWindow;
+		[SerializeField] private StartWindow _startWindow;
+		[SerializeField] private PauseHandler _pauseHandler;
 
-		public GameObject TapBlock   = null;
-		public WinWindow  WinWindow  = null;
-		public LoseWindow LoseWindow = null;
+		private int _ordersTarget = 0;
+		private bool _gameStarted = false;
 
+		public event Action TotalOrdersServedChanged;
 
-		int _ordersTarget = 0;
+		public static GameplayController Instance {get; private set;}
 
-		public int OrdersTarget {
-			get { return _ordersTarget; }
-			set {
+		public int TotalOrdersServed {get; private set;} = 0;
+
+		public int OrdersTarget
+		{
+			get {return _ordersTarget;}
+			set
+			{
 				_ordersTarget = value;
 				TotalOrdersServedChanged?.Invoke();
 			}
 		}
 
-		public int        TotalOrdersServed { get; private set; } = 0;
-
-		public event Action TotalOrdersServedChanged;
-
-		void Awake() {
-			if ( Instance != null ) {
+		private void Awake()
+		{
+			if (Instance != null)
 				Debug.LogError("Another instance of GameplayController already exists");
-			}
+
 			Instance = this;
 		}
 
-		void OnDestroy() {
-			if ( Instance == this ) {
+		private void Start()
+		{
+			Init();
+			OnGameStart();
+		}
+
+		private void OnDestroy()
+		{
+			if (Instance == this)
 				Instance = null;
-			}
 		}
 
-		void Init() {
-			TotalOrdersServed = 0;
-			Time.timeScale = 1f;
-			TotalOrdersServedChanged?.Invoke();
-		}
-
-		public void CheckGameFinish() {
-			if ( CustomersController.Instance.IsComplete ) {
+		public void CheckGameFinish()
+		{
+			if (CustomersController.Instance.IsComplete)
 				EndGame(TotalOrdersServed >= OrdersTarget);
-			}
-		}
-
-		void EndGame(bool win) {
-			Time.timeScale = 0f;
-			TapBlock?.SetActive(true);
-			if ( win ) {
-				WinWindow.Show();
-			} else {
-				LoseWindow.Show();
-			}
-		}
-
-		void HideWindows() {
-			TapBlock?.SetActive(false);
-			WinWindow?.Hide();
-			LoseWindow?.Hide();
 		}
 
 		[UsedImplicitly]
-		public bool TryServeOrder(Order order) {
-			if ( !CustomersController.Instance.ServeOrder(order) ) {
+		public bool TryServeOrder(Order order)
+		{
+			if (CustomersController.Instance.ServeOrder(order) == false)
 				return false;
-			}
 
 			TotalOrdersServed++;
 			TotalOrdersServedChanged?.Invoke();
 			CheckGameFinish();
+
 			return true;
 		}
 
-		public void Restart() {
+		public void Restart()
+		{
 			Init();
 			CustomersController.Instance.Init();
 			HideWindows();
+			_startWindow.Show();
 
-			foreach ( var place in FindObjectsByType<AbstractFoodPlace>(FindObjectsSortMode.None) ) {
+			foreach (AbstractFoodPlace place in FindObjectsByType<AbstractFoodPlace>(FindObjectsSortMode.None))
 				place.FreePlace();
-			}
 		}
 
-		public void CloseGame() {
+		public void CloseGame()
+		{
 #if UNITY_EDITOR
 			UnityEditor.EditorApplication.isPlaying = false;
 #else
 			Application.Quit();
 #endif
+		}
+
+		private void Init()
+		{
+			TotalOrdersServed = 0;
+			_pauseHandler.PauseGame();
+			TotalOrdersServedChanged?.Invoke();
+			_gameStarted = false;
+		}
+
+		private void OnGameStart()
+		{
+			if (_gameStarted == false)
+			{
+				_startWindow.Show();
+				_gameStarted = true;
+			}
+		}
+
+		private void EndGame(bool win)
+		{
+			_pauseHandler.PauseGame();
+			_tapBlock?.SetActive(true);
+
+			if (win)
+				_winWindow.Show();
+			else
+				_loseWindow.Show();
+		}
+
+		private void HideWindows()
+		{
+			_tapBlock?.SetActive(false);
+			_winWindow?.Hide();
+			_loseWindow?.Hide();
 		}
 	}
 }
