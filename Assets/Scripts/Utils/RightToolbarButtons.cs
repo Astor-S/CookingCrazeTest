bef7622760177d4f3c4a9ac7d3b0f578c7d0882b
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-06 17:28:01 +0300

    refactor: improving code style and code quality
    1) correction formatting  in class
    2) remove unused usings
    3) add consts
    4) improve variable naming
    5) general code readability improvements

--- Assets/Scripts/Utils/RightToolbarButtons.cs
+++ Assets/Scripts/Utils/RightToolbarButtons.cs
@@ -3,80 +3,99 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Text;
 using UnityEditor;
 using UnityEngine;
 using UnityToolbarExtender;
-using Debug = UnityEngine.Debug;
 
-namespace CookingPrototype.Utils {
+namespace CookingPrototype.Utils
+{
 	[InitializeOnLoad]
-	public static class RightToolbarButtons {
-		static RightToolbarButtons() {
+	public static class RightToolbarButtons
+	{
+		private const float PackProjectProgress = 0.5f;
+
+		private const string IgnoreDirectoryLibrary = "Library";
+		private const string IgnoreDirectoryTemp = "Temp";
+		private const string IgnoreDirectoryObj = "Obj";
+		private const string IgnoreDirectoryobj = "obj";
+		private const string IgnoreDirectoryIdea = ".idea";
+		private const string IgnoreDirectoryLogs = "Logs";
+
+		private const string SymbolAsterisk = "*";
+
+		static RightToolbarButtons()
+		{
 			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
 		}
 
-		static void OnToolbarGUI() {
+		static void OnToolbarGUI()
+		{
 			GUILayout.FlexibleSpace();
 
-			if ( GUILayout.Button(new GUIContent("Pack Project", "Pack zip archive with the project to send to Matryoshka Games"), EditorStyles.toolbarButton) ) {
-				try {
-					EditorUtility.DisplayProgressBar("Packing project", "Packing project to zip archive...", 0.5f);
+			if (GUILayout.Button(new GUIContent("Pack Project", "Pack zip archive with the project to send to Matryoshka Games"), EditorStyles.toolbarButton))
+				{
+				try
+				{
+					EditorUtility.DisplayProgressBar("Packing project", "Packing project to zip archive...", PackProjectProgress);
 					PackArchive();
 				}
-				finally {
+				finally
+				{
 					EditorUtility.ClearProgressBar();
 				}
 			}
 		}
 
-		static void PackArchive() {
+		static void PackArchive()
+		{
 			var projectPath = Directory.GetParent(Application.dataPath)?.FullName ?? string.Empty; 
 			var outputPath = Path.Combine(projectPath, "TestDeveloper.zip");
 
-			var ignoreDirectories = new[] {
-				"Library",
-				"Temp",
-				"Obj",
-				"obj",
-				".idea",
-				"Logs"
+			var ignoreDirectories = new[]
+			{
+				IgnoreDirectoryLibrary,
+				IgnoreDirectoryTemp,
+				IgnoreDirectoryObj,
+				IgnoreDirectoryobj,
+				IgnoreDirectoryIdea,
+				IgnoreDirectoryLogs
 			};
 			
 			var fullPathIgnoreDirectories = ignoreDirectories
-				.Select(x => Path.Combine(projectPath, x))
+				.Select(ignoreDirectory => Path.Combine(projectPath, ignoreDirectory))
 				.ToArray();
 
-			if ( File.Exists(outputPath) ) {
+			if (File.Exists(outputPath))
 				File.Delete(outputPath);
-			}
 			
 			using var archive = ZipFile.Open(outputPath, ZipArchiveMode.Create);
 			
-			foreach (var filePath in Directory.GetFiles(projectPath, "*", SearchOption.AllDirectories)) {
-				var anyIgnoredDirectory = fullPathIgnoreDirectories.Any(x => filePath.StartsWith(x));
-				if ( anyIgnoredDirectory ) {
+			foreach (var filePath in Directory.GetFiles(projectPath, SymbolAsterisk, SearchOption.AllDirectories))
+			{
+				var anyIgnoredDirectory = fullPathIgnoreDirectories.Any(ignoredDirectoryPath => filePath.StartsWith(ignoredDirectoryPath));
+
+				if ( anyIgnoredDirectory )
+				{
 					// ignore file
 					continue;
 				}
 				// Skip the output zip file itself
-				if (Path.GetFullPath(filePath) == outputPath) {
+				if (Path.GetFullPath(filePath) == outputPath)
 					continue;
-				}
 				
 				var relativePath = Path.GetRelativePath(projectPath, filePath);
 				archive.CreateEntryFromFile(filePath, relativePath);
 			}
-			
-			
+						
 			TryOpenDirectory(projectPath);
 		}
 
-		static void TryOpenDirectory(string path) {
-			try {
-				if (string.IsNullOrEmpty(path)) {
+		static void TryOpenDirectory(string path)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty(path))
 					throw new ArgumentException("Path cannot be null or empty", nameof(path));
-				}
 
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
 				Process.Start("explorer.exe", path.Replace("/", "\\"));
@@ -85,7 +104,9 @@ namespace CookingPrototype.Utils {
 #else
     Debug.LogError("Opening directories is not supported on this platform.");
 #endif
-			} catch (Exception) {
+			}
+			catch (Exception)
+			{
 				// Do nothing
 			}
 		}
