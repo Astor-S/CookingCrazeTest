9e0065c781b68a2bc827f7a596f464b773b26cf8
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-06 16:59:53 +0300

    refactor: improving code style and code quality
    1) correction formatting  in class
    2) explicitly specifying access modifiers
    3) specifying an explicit type instead of "var"
    4) replacing the operator "!" with an explicit instruction
    5) general code readability improvements

--- Assets/Scripts/Kitchen/OrderServer.cs
+++ Assets/Scripts/Kitchen/OrderServer.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
-
 using CookingPrototype.Controllers;
-
 using JetBrains.Annotations;
 
-namespace CookingPrototype.Kitchen {
+namespace CookingPrototype.Kitchen
+{
 	[RequireComponent(typeof(OrderPlace))]
-	public sealed class OrderServer : MonoBehaviour {
-		OrderPlace _orderPlace;
+	public sealed class OrderServer : MonoBehaviour
+	{
+		private OrderPlace _orderPlace;
 
-		void Start() {
+		private void Start()
+		{
 			_orderPlace = GetComponent<OrderPlace>();
 		}
 
 		[UsedImplicitly]
-		public void TryServeOrder() {
-			var order = OrdersController.Instance.FindOrder(_orderPlace.CurOrder);
-			if ( (order == null) || !GameplayController.Instance.TryServeOrder(order) ) {
+		public void TryServeOrder()
+		{
+			Order order = OrdersController.Instance.FindOrder(_orderPlace.CurOrder);
+
+			if ((order == null) || GameplayController.Instance.TryServeOrder(order) == false)
 				return;
-			}
 
 			_orderPlace.FreePlace();
 		}
