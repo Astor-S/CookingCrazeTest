86b19e62989c3af199c8891b3653ecf8b2673840
Astor-S <154661724+Astor-S@users.noreply.github.com>
2025-07-06 16:25:52 +0300

    refactor: improving code style and code quality
    1) correction formatting  in class
    2) replacing public fields with private and creating properties
    3) general code readability improvements

--- Assets/Scripts/Kitchen/Order.cs
+++ Assets/Scripts/Kitchen/Order.cs
@@ -1,25 +1,32 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace CookingPrototype.Kitchen {
-	public sealed class Order {
-		public class OrderFood {
+namespace CookingPrototype.Kitchen
+{
+	public sealed class Order
+	{
+		public class OrderFood
+		{
 			public string Name  { get; } = null;
 			public string Needs { get; } = null;
 
-			public OrderFood(string name, string needs) {
+			public OrderFood(string name, string needs)
+			{
 				Name  = name;
 				Needs = needs;
 			}
 		}
 
-		public readonly string               Name;
-		public ReadOnlyCollection<OrderFood> Foods { get { return _foods.AsReadOnly(); } }
+		private readonly string _name;
 
-		List<OrderFood> _foods;
+		private readonly List<OrderFood> _foods;
 
-		public Order(string name, List<OrderFood> foods) {
-			Name   = name;
+		public ReadOnlyCollection<OrderFood> Foods => _foods.AsReadOnly();
+		public string Name => _name;
+
+		public Order(string name, List<OrderFood> foods)
+		{
+			_name   = name;
 			_foods = foods;
 		}
 	}
